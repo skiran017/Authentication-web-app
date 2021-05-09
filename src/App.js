8fe3885f6e1b8e0c725d773f96603677efdc76e4
skiran017 <skiran017@gmail.com>
2021-05-09 08:39:26 +0530

    final
    -auth0 init
    -useContext hook
    -privateRoute
    -get token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SiteHeader from './components/SiteHeader';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth0 } from "./contexts/auth0.context";
+import PrivateRoute from "./components/PrivateRoute";
+import SiteHeader from "./components/SiteHeader";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import "./App.css";
+import { useEffect } from "react";
 
 export default function App() {
+  const { getToken } = useAuth0();
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    const token = await getToken();
+    console.log(token);
+    const response = await fetch(`http://example.com/api`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+
+    //we have data
+  }
+
   return (
     <Router>
       <div className="app">
@@ -14,9 +34,9 @@ export default function App() {
 
         {/* routes */}
         <Switch>
-          <Route path="/dashboard">
+          <PrivateRoute path="/dashboard">
             <Dashboard />
-          </Route>
+          </PrivateRoute>
           <Route path="/" exact={true}>
             <Home />
           </Route>
